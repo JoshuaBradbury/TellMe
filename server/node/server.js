7ce38798270ff89ec06a6ebe0f611a29cb6fa60c
Joshua Bradbury <Joshua.Bradbury@protonmail.com>
2018-02-27 20:27:06 +0000

    Added d SSL certificate so that requests can be achieved using HTTPS

--- server/node/server.js
+++ server/node/server.js
@@ -1,27 +1,52 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var multer = require('multer');
+var multer = require("multer");
+var morgan = require("morgan");
+var https = require("https");
+var http = require("http");
+var fs = require("fs");
 
 var upload = multer();
 var app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ ezxtended: true }));
+var options = {
+	key: fs.readFileSync("/etc/letsencrypt/live/tellme.newagedev.co.uk/privkey.pem"),
+	cert: fs.readFileSync("/etc/letsencrypt/live/tellme.newagedev.co.uk/fullchain.pem")
+};
+
+app.disable('x-powered-by');
 
 var router = express.Router();
 
-router.post("/cat", upload.array(), (req, res, next) => {
-	if (req.body.text) {
-		console.log("Text: " + req.body.text);
-		res.status(200).json({"cat-text": req.body.text});
+router.use(morgan("dev"));
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
+
+router.use(function(req, res, next) {
+	res.header('Access-Control-Allow-Credentials', true);
+	res.header('Access-Control-Allow-Origin', "*");
+	res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+	next();
+}).options('*', function (req, res, next) {
+	res.end();
+});
+
+router.post("/group", upload.array(), (req, res, next) => {
+	if (req.secure && req.accepts('application/json')) {
+		if (req.body.text) {
+			res.status(200).json({"cat-text": req.body.text});
+		} else {
+			res.sendStatus(400);
+		}
 	} else {
-		console.log("Request missing \"text\"");
-		res.status(400).json({"error": "Sorry parameter \"text\" required in json object"})
+		res.sendStatus(406);
 	}
 });
 
 app.use(router);
 
-app.listen(8080, function () {
-  console.log("Server listening on port 8080");
+https.createServer(options, app).listen(443, function() {
+	console.log("Express is listening on port 443");
 });
