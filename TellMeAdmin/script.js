1fba226dc1a788f037434b56b56b5be631c875c9
Teodora Milenkovic <teodora.milenkovic@kcl.ac.uk>
2018-02-27 10:58:16 +0100

    New announcement div done

--- TellMeAdmin/script.js
+++ TellMeAdmin/script.js
@@ -1,15 +1,33 @@
-function validateForm() {
-    var postBox = document.getElementsByClassName('pop-container');
-    var popupBtn = document.getElementsByClassName('pop-input-mysize');
-    postBox.style.display = "none";
 
-    popupBtn.onclick = function() {
-      postBox.style.display = "block";
-    }
+var newAnnouncementBtn = document.getElementById('postBtn');
 
-    window.onclick = function(event){
-      if (event.target == postBox){
-        postBox.style.display = "none";
-      }
-    }
+newAnnouncementBtn.onclick = function() {
+  var newA = document.getElementById('popcontainer');
+  if (newA.style.display !== 'none') {
+    newA.style.display = 'none';
+  } else {
+    newA.style.display = 'block';
+  }
+  document.getElementById("announcementsBoard").style.display="none";
+  document.getElementById("postBtn").style.opacity = "0.5";
+  document.getElementById("overlay").style.display = "block";
+  $('body').click(function () {
+     $('div#popcontainer').hide();
+ });
 }
+
+
+$(document)
+  .one('focus.autoExpand', 'textarea.autoExpand', function() {
+    var savedValue = this.value;
+    this.value = '';
+    this.baseScrollHeight = this.scrollHeight;
+    this.value = savedValue;
+  })
+  .on('input.autoExpand', 'textarea.autoExpand', function() {
+    var minRows = this.getAttribute('data-min-rows') | 0,
+      rows;
+    this.rows = minRows;
+    rows = Math.ceil((this.scrollHeight - this.baseScrollHeight) / 16);
+    this.rows = minRows + rows;
+  });
