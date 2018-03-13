8ffcb3f151afcb76c92028d6ac8d12cf736643f1
LovelyCodeMonkey <mahyad.ghassemi@hangoutapp.tech>
2018-03-13 13:31:21 +0000

    created the initial page of the announcement page

--- TellMe/ViewController.swift
+++ TellMe/ViewController.swift
@@ -8,22 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     let msalHandler = MSALHandler()
+    @IBOutlet weak var announcementTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        announcementTableView.delegate = self
+        announcementTableView.dataSource = self
+        
+    }
     
     override func viewDidAppear(_ animated: Bool) {
         msalHandler.login()
         
     }
     
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
     }
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
