80d0f5b1b439bf62cdbbff90087c14cafd08d3c5
Sophia Kalanovska <sophiakalanovska@sophias-mbp.home>
2018-03-25 23:31:12 +0100

    tests set up

---
+++ TellMeTests/TestAnnouncmentViewController.swift
@@ -0,0 +1,28 @@
+//
+//  TestAnnouncmentViewController.swift
+//  TellMeTests
+//
+//  Created by Sophia Kalanovska on 25/03/2018.
+//  Copyright © 2018 Sophia Kalanovska. All rights reserved.
+//
+
+import XCTest
+@testable import TellMe
+class TestAnnouncmentViewController: XCTestCase {
+    let announce = AnnouncementController()
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testSetupSideMenu(){
+        
+    }
+    
+    
+}
